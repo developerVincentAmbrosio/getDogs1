18055d557dd341d06986c34b90925d43b064a804
Vincent Ambrosio <vince.ambrosio@gmail.com>
2020-10-23 12:22:20 -0400

    made changes to JS file after mentor session

--- consoleDogs.js
+++ consoleDogs.js
@@ -1,26 +1,33 @@
-function getDogPics () {
-  fetch('https://dog.ceo/api/breeds/image/random')
+function getDogPics (numberOfDogPics) {
+  fetch(`https://dog.ceo/api/breeds/image/random/${numberOfDogPics}`)
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
-  // .catch(error => alert('Something went wrong, please try again later'));
+    .then(responseJson => {
+      renderImageUrls(responseJson.message);
+    })
+  //  .then(responseJson => console.log(responseJson)) needed for #1
+    .catch(error => alert('Something went wrong, please try again later'));
 }
 
-function gotDogPicsListener(number) {
-  $('#button').submit(event => {
+function getDogPicsListener() {
+  $('form').submit(event => {
     event.preventDefault();
-//    const numberOfDogPics = $('#dogs').val();
-//    numberBetweenOneAndFifty();
-    getDogPics();
+    const numberOfDogPics = $('.js-number-of-dogs').val();
+//    console.log(numberOfDogPics);
+    getDogPics(numberOfDogPics);
   });
 }
 
-// function numberBetweenOneAndFifty() {
-//   if (numberOfDogPics > 50) {
-//     alert('Please pick a number between 1 and 50');
-//   }
-// }
+function renderImageUrls(urls) {
+ console.log(urls);
+//loop through JSON data
+//take response json, for loop call on line 4, call dog pic template function, use jquery to add to element in html file
+
+}
+
+function dogPicTemplate(imageUrl) {
+  return `<img src=${imageUrl}/>`;
+}
 
 $(function() {
-  console.log('ready');
-  gotDogPicsListener();
+  getDogPicsListener();
 });
