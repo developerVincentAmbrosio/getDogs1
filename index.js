4702c2e8930719b92128e634c8cf2dfac8b6d7eb
Vincent Ambrosio <vince.ambrosio@gmail.com>
2020-10-23 15:03:27 -0400

    worked on styles

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 function getDogPics (numberOfDogPics) {
   fetch(`https://dog.ceo/api/breeds/image/random/${numberOfDogPics}`)
     .then(response => response.json())
-    .then(responseJson => {
-      renderImageUrls(responseJson.message);
-    })
-  //  .then(responseJson => console.log(responseJson)) needed for #1
+    .then(responseJson => console.log(responseJson))
     .catch(error => alert('Something went wrong, please try again later'));
 }
 
@@ -12,20 +9,8 @@ function getDogPicsListener() {
   $('form').submit(event => {
     event.preventDefault();
     const numberOfDogPics = $('.js-number-of-dogs').val();
-//    console.log(numberOfDogPics);
     getDogPics(numberOfDogPics);
   });
-}
-
-function renderImageUrls(urls) {
- console.log(urls);
-//loop through JSON data
-//take response json, for loop call on line 4, call dog pic template function, use jquery to add to element in html file
-
-}
-
-function dogPicTemplate(imageUrl) {
-  return `<img src=${imageUrl}/>`;
 }
 
 $(function() {
